5b199aa50881f262c7993e7217e31647e01e050f
obelisk477 <cwbatte2@gmail.com>
2023-05-21 16:58:05 -0400

    Added and finished README; added 'catch' logic to inquirer prompt

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const inquirer = require('inquirer')
 const { generateInnerSVG } = require('./lib/generateSVG')
 const fs = require('fs');
 
+// Get user input
 inquirer
     .prompt([
         {type: 'input',
         message: 'What text should go in the icon? (up to 3 letters)',
         name: 'logoText',
+        // Make logo text completely uppercase
         filter: (ans) => {
             return ans.toUpperCase()
         } 
@@ -26,11 +28,21 @@ inquirer
         }
     ])
     .then(results => {
+        // Get inner SVG shape and text
         let innerSVG = generateInnerSVG(results.logoText, results.textColor, results.shapeColor, results.shape)
-        let totalSVG = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">${innerSVG}</svg>`
+
+        // Total SVG text; strange indentation to help with file indentation & formatting
+        let totalSVG = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+    ${innerSVG}
+</svg>`
         fs.writeFile('./examples/logo.svg', totalSVG, err => {
             err? console.error(err) : console.log("\nGenerated logo.svg")
         })
     })
+    .catch(err => {
+        console.log(err)
+        console.log('Something went wrong. Please try again once errors are resolved')
+    })
+
 
     
